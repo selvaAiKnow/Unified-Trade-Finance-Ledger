694ada3e416de58808352add06c3eaa74958a853
selva <selva@aiknow.in>
2026-07-27 13:26:15 +0530

    Scaffold blockchain-layer Ktor project with a health endpoint

---
+++ CorDapp/blockchain-layer/src/test/kotlin/com/utfl/blockchainlayer/ApplicationTest.kt
@@ -0,0 +1,18 @@
+package com.utfl.blockchainlayer
+
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+    @Test
+    fun `health endpoint returns ok`() = testApplication {
+        application { module() }
+        val response = client.get("/health")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("""{"status":"ok"}""", response.bodyAsText())
+    }
+}
